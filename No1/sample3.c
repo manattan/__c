f8d4179a18bb331fdaa17fecab4c2cc62f6f03dd
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-10-05 11:06:08 +0900

    sample3の作成

---
+++ No1/sample3.c
@@ -0,0 +1,29 @@
+# include "stdafx.h"
+# include "stdio.h"
+
+int main(void) {
+    double x,y;
+    FILE *fp, *fpp;
+    int err,err2; // errno_t が望ましい...
+
+    err = fopen_s(&fp, "sample3_1.txt","a"); // 追加書き込み
+    err2 = fopen_s(&fpp, "sample3_2.txt", "x"); // 上書き
+
+    if (err!=0 | err2!=0) {
+        printf("ファイルを読み込めませんでした"); //オープンモードがrでないため不要
+    } else {
+        printf("x= ");
+        scanf("%lf",&x);
+
+        printf("y= ");
+        scanf("%lf",&y);
+
+        fprintf(fp,"%f\t%f",x,y);
+        fprintf(fpp,"%f\t%f",x,y);
+
+        fclose(fp);
+        fclose(fpp);
+    }
+}
+
+// 最初は同じように書き込まれるが、2回目以降は異なる(モードの違い)
