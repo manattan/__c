7a364ec94c89cff91bb044f8e57bce84a35914ca
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-10-05 09:53:20 +0900

    sample1訂正

--- No1/sample1.c
+++ No1/sample1.c
@@ -1,8 +1,10 @@
 # include "stdio.h"
 
 int main(void){
-    printf("start\t");
-    printf("test\n");
+    printf("start\t"); // tab
+    printf("test\n"); // 改行
     printf("end\n");
     return 0; // 不要
 }
+
+// 実行はvsCodeにて "gcc -o a.out ファイル名.c" としている
