cf044f55718675e043331e6a14e3b6b301720cb7
manattan <mogimogi0529@eis.hokudai.ac.jp>
2020-10-05 09:51:11 +0900

    sample1とsample2作成

---
+++ No1/sample1.c
@@ -0,0 +1,8 @@
+# include "stdio.h"
+
+int main(void){
+    printf("start\t");
+    printf("test\n");
+    printf("end\n");
+    return 0; // 不要
+}
---
+++ No1/sample2.c
@@ -0,0 +1,10 @@
+# include "stdio.h"
+
+int main(void){
+    double x; // 実数xを定義
+    printf("x=");
+    scanf("%lf",&x); // scanf_sとする必要はない
+
+    printf("x^2 = %10.5f\n", x*x); // 文字幅10,小数点以下5桁で実数表示
+    printf("x^2 = %d\n", (int)(x*x)); // intで出力
+}
